7840c88d57a858ddb75cb8d1027182c346dae3ca
opielapatryk <xpatrykopiela@gmail.com>
2023-07-26 13:21:59 +0200

    comments

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import tkinter as tk 
 
-player = "x"
-previous_moves = []
+player = "x" #we use this to change player 
+previous_moves = [] #here we will collect all moves
+#later I will check if previous moves texts are...... then.......
 
-def show_msg(row, col):
-    global previous_moves
+def show_msg(row, col): #function for display the current moves
+    global previous_moves 
     global player
-    button = buttons[row][col]
-    if (row, col) not in previous_moves:
-        previous_moves.append((row, col))
-        button["text"] = player
-        if player == "x":
+    button = buttons[row][col] 
+    if (row, col) not in previous_moves: # check if current move was previously selected
+        previous_moves.append((row, col)) # if not, then add this to our list with previous moves
+        button["text"] = player # change text ! 
+        if player == "x": 
             player = "o"
         else:
             player = "x"
-        print(list(previous_moves))
+        print(list(previous_moves)) #later I will delete this line
         
-    
-
-
 frame = tk.Tk()
 buttons = []
 
