837c16da00011652eed8224c5af3e2da40e153b4
opielapatryk <xpatrykopiela@gmail.com>
2023-07-26 16:47:26 +0200

    almonst done

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import tkinter as tk 
+from tkinter import messagebox
 
 player = "x" #we use this to change player 
+moves = 0 
 previous_moves = [] #here we will collect all moves
 #later I will check if previous moves texts are...... then.......
 
 def show_msg(row, col): #function for display the current moves
     global previous_moves 
     global player
+    global moves
     button = buttons[row][col] 
     if (row, col) not in previous_moves: # check if current move was previously selected
+        moves = moves + 1 
         previous_moves.append((row, col)) # if not, then add this to our list with previous moves
         button["text"] = player # change text ! 
         if player == "x": 
             player = "o"
         else:
             player = "x"
-        print(list(previous_moves)) #later I will delete this line
+    check_win()
+
+def check_win():
+    global buttons
+    global moves
+    if buttons[0][0]['text'] == 'x' and buttons[1][0]['text'] == 'x' and buttons[2][0]['text'] == 'x' or buttons[0][1]['text'] == 'x' and buttons[1][1]['text'] == 'x' and buttons[2][1]['text'] == 'x' or buttons[0][2]['text'] == 'x' and buttons[1][2]['text'] == 'x' and buttons[2][2]['text'] == 'x' or buttons[0][0]['text'] == 'x' and buttons[0][1]['text'] == 'x' and buttons[0][2]['text'] == 'x' or buttons[1][0]['text'] == 'x' and buttons[1][1]['text'] == 'x' and buttons[1][2]['text'] == 'x' or buttons[2][0]['text'] == 'x' and buttons[2][1]['text'] == 'x' and buttons[2][2]['text'] == 'x' or buttons[0][0]['text'] == 'x' and buttons[1][1]['text'] == 'x' and buttons[2][2]['text'] == 'x' or buttons[2][0]['text'] == 'x' and buttons[1][1]['text'] == 'x' and buttons[0][2]['text'] == 'x':
+        winner = 'x'
+        button.after(100, lambda: messagebox.showinfo("showinfo", f"{winner} win!"))
+    elif buttons[0][0]['text'] == 'o' and buttons[1][0]['text'] == 'o' and buttons[2][0]['text'] == 'o' or buttons[0][1]['text'] == 'o' and buttons[1][1]['text'] == 'o' and buttons[2][1]['text'] == 'o' or buttons[0][2]['text'] == 'o' and buttons[1][2]['text'] == 'o' and buttons[2][2]['text'] == 'o' or buttons[0][0]['text'] == 'o' and buttons[0][1]['text'] == 'o' and buttons[0][2]['text'] == 'o' or buttons[1][0]['text'] == 'o' and buttons[1][1]['text'] == 'o' and buttons[1][2]['text'] == 'o' or buttons[2][0]['text'] == 'o' and buttons[2][1]['text'] == 'o' and buttons[2][2]['text'] == 'o' or buttons[0][0]['text'] == 'o' and buttons[1][1]['text'] == 'o' and buttons[2][2]['text'] == 'o' or buttons[2][0]['text'] == 'o' and buttons[1][1]['text'] == 'o' and buttons[0][2]['text'] == 'o':
+        winner = 'o'
+        button.after(100, lambda: messagebox.showinfo("showinfo", f"{winner} win!"))
+    elif moves == 9:
+        winner = 'Both of you '
+        button.after(100, lambda: messagebox.showinfo("showinfo", f"{winner} win!"))
+
         
 frame = tk.Tk()
 buttons = []
